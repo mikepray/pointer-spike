b5b3e6ede7229e88e8edacbb7bf2abb5b20a8f8f
mike.pray <mwpray@gmail.com>
2022-03-21 22:27:28 -0500

    adding ws

--- pointer-backend/src/server.ts
+++ pointer-backend/src/server.ts
@@ -3,11 +3,13 @@ import issuesRoutes from './routes/issue.router';
 import { json } from 'body-parser';
 import { connectToCouchbase } from './dao/issue.dao';
 import { Cluster } from 'couchbase';
+import { WebSocket, WebSocketServer } from 'ws';
+import http, { createServer } from 'http';
 
 const PORT = 8080;
 
-export let couchbaseConnection:Promise<Cluster>;
-couchbaseConnection = connectToCouchbase();
+export let couchbaseConnection:Promise<Cluster> = connectToCouchbase();
+
 //Create an app
 const app = express();
 
@@ -25,7 +27,35 @@ app.use((err: Error, req: Request, res:Response, next: NextFunction) => {
     res.status(500).json({message: err.message});
 });
 
-app.listen(PORT);
+const server = createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', function (ws, request) {
+    
+    ws.on('message', function (message) {
+       
+        console.log(`Received message ${message} `);
+    });
+    
+    ws.on('close', function () {
+        //   map.delete(userId);
+    });
+});
+
+server.listen(PORT);
 
 //Listen port
-console.log(`Running on port ${PORT}`);
+console.log(`HTTP and WebSocket Server Running on port ${PORT}`);
+
+// WebSocket Server
+// const wss = new WebSocketServer({ port: 8081 });
+
+// wss.on('connection', function connection(ws) {
+    //   ws.on('message', function message(data) {
+        //     console.log('received: %s', data);
+        //   });
+        
+        //   ws.send('something');
+        // });
+
+// See this for eventual Rest->WS upgrade https://github.com/websockets/ws/blob/master/examples/express-session-parse/index.js
